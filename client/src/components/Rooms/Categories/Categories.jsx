db47b5ac8853566336ef1044878c93d7d1785772
Shraban Chakma <shrabanchakma328@gmail.com>
2024-03-12 23:22:35 +0600

    different style for active category

--- client/src/components/Rooms/Categories/Categories.jsx
+++ client/src/components/Rooms/Categories/Categories.jsx
@@ -1,16 +1,21 @@
+import { useSearchParams } from "react-router-dom";
 import Container from "../../Shared/Container";
 import { categories } from "./CategoriesData";
 import CategoryBox from "./CategoryBox";
 
 const Categories = () => {
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
+  console.log(category);
   return (
     <Container>
       <div className="mt-8 flex items-center justify-between overflow-x-auto ">
-        {categories.map((category) => (
+        {categories.map((item) => (
           <CategoryBox
-            key={category.label}
-            label={category.label}
-            icon={category.icon}
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            category={category === item.label}
           />
         ))}
       </div>
--- client/src/components/Rooms/Categories/CategoryBox.jsx
+++ client/src/components/Rooms/Categories/CategoryBox.jsx
@@ -1,6 +1,26 @@
-const CategoryBox = ({ label, icon: Icon }) => {
+import { useNavigate, useSearchParams } from "react-router-dom";
+import qs from "query-string";
+const CategoryBox = ({ label, icon: Icon, category }) => {
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+  const handleClick = () => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+      const updatedQuery = { ...currentQuery, category: label };
+      const url = qs.stringifyUrl({ url: "/", query: updatedQuery });
+      navigate(url);
+    }
+  };
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer">
+    <div
+      onClick={handleClick}
+      className={`${
+        category
+          ? "border-b-neutral-800 text-neutral-800"
+          : "border-transparent text-neutral-500"
+      } flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer`}
+    >
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>
     </div>
